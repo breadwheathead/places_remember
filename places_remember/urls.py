862f1655bd962621cadeb40cfdbff0d26f5a8518
Bread <breadwheathead@gmail.com>
2022-12-29 18:41:20 +0700

    add logout

--- places_remember/urls.py
+++ places_remember/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 
 from mainapp.views import MainPageView
 
@@ -24,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MainPageView.as_view(), name='main'),
     path('', include('social_django.urls', namespace='social')),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
 
 if settings.DEBUG:
