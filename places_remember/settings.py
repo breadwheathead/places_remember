05f3d631a43148c7dffb798c7bc312a693b42e57
Bread <breadwheathead@gmail.com>
2022-12-29 17:44:13 +0700

    load static

--- places_remember/settings.py
+++ places_remember/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os
 import environ
 from pathlib import Path
 
@@ -122,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
