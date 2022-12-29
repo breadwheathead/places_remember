868ae74d32c7a1c55279f5a719cc80c9f08b16ad
Bread <breadwheathead@gmail.com>
2022-12-29 20:49:52 +0700

    update Remember model fields

--- remembersapp/models.py
+++ remembersapp/models.py
@@ -5,8 +5,8 @@ from mainapp.models import User
 
 class Remember(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=256, unique=True)
-    description = models.TextField(max_length=512, blank=True)
+    title = models.CharField(max_length=128, unique=True)
+    description = models.CharField(max_length=512, blank=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
     create_timestamp = models.DateTimeField(auto_now_add=True)
