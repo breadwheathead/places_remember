f24d2d221f739a8d57794c175b04f3ea70a5094e
Bread <breadwheathead@gmail.com>
2022-12-29 18:50:46 +0700

    get vk-profile avatar

--- places_remember/settings.py
+++ places_remember/settings.py
@@ -135,3 +135,15 @@ SOCIAL_AUTH_VK_OAUTH2_SECRET = 'ffOLc3lHciy1zsqKgtx9'
 
 SOCIAL_AUTH_VK_OAUTH2_IGNORE_DEFAULT_SCOPE = True
 SOCIAL_AUTH_VK_OAUTH2_SCOPE = ['email']
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.create_user',
+    'mainapp.pipeline.save_avatar',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
