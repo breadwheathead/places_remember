b829507b7467f6cb08ed0207253f554cefdc6b2c
Bread <breadwheathead@gmail.com>
2022-12-29 18:32:49 +0700

    add url path  vk auth

--- places_remember/urls.py
+++ places_remember/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from mainapp.views import MainPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MainPageView.as_view(), name='main'),
+    path('', include('social_django.urls', namespace='social')),
 ]
 
 if settings.DEBUG:
