c2094df00350566161470e3f8351367c5c00c096
Bread <breadwheathead@gmail.com>
2022-12-29 17:27:41 +0700

    create main template

--- places_remember/urls.py
+++ places_remember/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mainapp.views import MainPageView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', MainPageView.as_view(), name='main'),
 ]
